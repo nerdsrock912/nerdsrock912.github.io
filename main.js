6e4ca3529b6ae3a7aedac4de4b32ad7cfdf4f920
Dr. Creeperstone <videogamemaster_12@hotmail.com>
2019-08-11 01:31:07 -0400

    Organize files; create contact page

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+window.onload = function() {
+    var footer = document.getElementById("footer-text");
+    var d = new Date();
+    var year = d.getUTCFullYear();
+    var text = "If I had a copyright...&copy; Dr. Creeperstone Agency " + year;
+    footer.innerHTML = text;
+}
